6f710e92d6b08499f7f62acfa866b8df3a794ef7
Felix-Florian Flesch <felix-florian.flesch@lancom.de>
2020-10-13 11:30:34 +0200

    added basic pages and shared components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { FeedPageComponent } from './pages/feed-page/feed-page.component';
+import { CreatePostPageComponent } from './pages/create-post-page/create-post-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
+import { GroupPageComponent } from './pages/group-page/group-page.component';
+import { HeaderComponent } from './shared-components/header/header.component';
+import { PostComponent } from './shared-components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    FeedPageComponent,
+    CreatePostPageComponent,
+    SearchPageComponent,
+    UserPageComponent,
+    GroupPageComponent,
+    HeaderComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
