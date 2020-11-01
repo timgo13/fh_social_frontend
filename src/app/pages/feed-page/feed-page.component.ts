a6a542141864af2f75881d43460c63a072607f3c
Felix Flesch <mail@felixflesch.de>
2020-11-01 17:26:09 +0100

    header page highlight

--- src/app/pages/feed-page/feed-page.component.ts
+++ src/app/pages/feed-page/feed-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PostDto } from '../../services/dto/post.dto';
 import { AuthService } from '../../services/auth.service';
 import { PostService } from '../../services/post.service';
+import { CurrentPage } from 'src/app/shared-components/header/header-current-page.enum';
 
 @Component({
   selector: 'app-feed-page',
@@ -9,6 +10,8 @@ import { PostService } from '../../services/post.service';
   styleUrls: ['./feed-page.component.scss']
 })
 export class FeedPageComponent implements OnInit {
+
+  CurrentPage: any = CurrentPage;
 
   posts: PostDto[] = [
     {id: 1, content: 'Content Post 1', createdDate: new Date(), creatorId: 1, creatorName: 'Name', childPost: {id: 2, content: 'Content Post 2', createdDate: new Date(), creatorId: 1, creatorName: 'Name'}},
