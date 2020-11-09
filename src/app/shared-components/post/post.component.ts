94d579fa043f22026b1830e710f08dc365f855bc
Felix Flesch <mail@felixflesch.de>
2020-11-09 14:54:37 +0100

    share post wip

--- src/app/shared-components/post/post.component.ts
+++ src/app/shared-components/post/post.component.ts
@@ -1,15 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PostDto } from '../../services/dto/post.dto';
+import { AuthService } from '../../services/auth.service';
+import {CreatePostDto} from '../../services/dto/create-post.dto';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
   @Input() post: PostDto;
+  @Input() showShareButton = true;
 
   shareModalActive = false;
+  privateShare = true;
+
+  currentDate: Date = new Date();
+  authenticatedUserUsername = this.authService.getAuthenticatedUserUsername();
+  createPostDto: CreatePostDto = {} as CreatePostDto;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    window.setInterval(() => {
+      this.currentDate = new Date();
+    }, 1000);
+  }
 
   onShareClick(): void {
     this.shareModalActive = true;
@@ -18,4 +34,9 @@ export class PostComponent {
   onCloseModalClick(): void {
     this.shareModalActive = false;
   }
+
+  onSubmitSharePostClick(): void { // TODO
+
+  }
+
 }
--- src/app/shared-components/post/post.module.ts
+++ src/app/shared-components/post/post.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './post.component';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -10,6 +12,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     RouterModule,
+    TranslateModule,
+    FormsModule,
   ],
   exports: [PostComponent]
 })
